62dbf3e7d6348758aa2d26dd0b76917f0f650f5f
AvanMyers <smail1341@gmail.com>
2022-08-08 16:25:22 +0300

    Shop

---
+++ Shop/Shop/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int gold;
+            int crystalPrice = 10;
+            int purchasedСrystal;
+            bool enoughGold;
+
+            Console.Write("Введите количество золотых монет: ");
+            gold = Convert.ToInt32(Console.ReadLine());
+            Console.Write($"У вас {gold} монет. Кристал стоит {crystalPrice}, " +
+                $"сколько кристалов вы хотели бы купить? ");
+            purchasedСrystal = Convert.ToInt32(Console.ReadLine());
+
+            enoughGold = gold >= purchasedСrystal * crystalPrice;
+            purchasedСrystal *= Convert.ToInt32(enoughGold);
+
+            gold -= purchasedСrystal * crystalPrice;
+
+            Console.WriteLine($"Теперь у вас {gold} золотых монет и {purchasedСrystal} кристалов");
+            Console.ReadKey();
+
+        }
+    }
+}
